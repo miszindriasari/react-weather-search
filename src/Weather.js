53e091cbc939b6fd98ea502b954ab8d7c01eb7cc
miszindriasari <82860575+miszindriasari@users.noreply.github.com>
2022-04-09 00:02:30 +0200

    added weather component

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>Amsterdam</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Clousy</li>
+      </ul>
+    </div>
+  );
+}
