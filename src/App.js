4fd5b16e205fbc2c19ae9bbb0d440beddb12444d
miszindriasari <82860575+miszindriasari@users.noreply.github.com>
2022-04-08 23:43:57 +0200

    added github url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by Tessa and is {""}
+        <a href="https://reactweathersearchapp.netlify.app/" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
