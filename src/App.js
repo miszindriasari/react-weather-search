680af9a3bf650b62525dee41366b8f50de4564b2
miszindriasari <82860575+miszindriasari@users.noreply.github.com>
2022-04-08 23:54:04 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by Tessa and is {""}
-        <a href="https://reactweathersearchapp.netlify.app/" target="_blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by Tessa and is {""}
+          <a href="https://reactweathersearchapp.netlify.app/" target="_blank">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
