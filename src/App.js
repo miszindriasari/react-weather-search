32623b2dfe38e35e340fe9966428d2ae9884e209
miszindriasari <82860575+miszindriasari@users.noreply.github.com>
2022-04-10 19:37:15 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ export default function App() {
       <div className="container">
         <Weather />
         <footer>
-          This project was coded by Tessa and is {""}
+          This project was coded by{" "}
+          <a href="https://www.indriasariphotography.com" target={"_blank"}>
+            Tessa
+          </a>{" "}
+          and is {""}
           <a href="https://reactweathersearchapp.netlify.app/" target="_blank">
             open-sourced on Github
           </a>
