d3061c043382b4a9fe0b027b215bd2e504517f2f
miszindriasari <82860575+miszindriasari@users.noreply.github.com>
2022-04-23 02:04:20 +0200

    basic styling css

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,16 @@
 import React from "react";
+import axios from "axios";
 import "./Weather.css";
+import { useState } from "react";
 
 export default function Weather() {
+  const [temperature, setTemperature] = useState(null);
+  function handleResponse(response) {}
+
+  const apiKey = "23a4a4f5340f54d85e35ee12e0d09bb3";
+  let city = "London";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={apiKey}&units=metric`;
+  axios.get.apply(apiUrl).then(handleResponse);
   return (
     <div className="Weather">
       <form>
@@ -11,11 +20,16 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="row">
             <div className="col-3">
-              <input type="submit" value="Search" className="btn btn-primary" />
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
         </div>
@@ -25,13 +39,19 @@ export default function Weather() {
         <li>Wednesday 07:00am</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
-          />
-          6°C
+          <div className="clearfix">
+            <div className="float-left">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="Mostly Cloudy"
+                className="float-left"
+              />
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
